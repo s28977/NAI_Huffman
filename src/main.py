621e5e6168dea515a3b44fc26215618cc7d6fdff
Jan Musial <jasiolix@gmail.com>
2024-06-09 01:50:38 +0200

    Created the main code solving this task and tested it on 2 examples from lecture and tutorial

--- src/main.py
+++ src/main.py
@@ -14,3 +14,31 @@ def map_to_subtrees_list(sequence):
     for i, e in enumerate(char_count_dict.items()):
         subtrees.append(Subtree(Symbol(e[0], i, e[1])))
     return subtrees
+
+
+def encode(sequence, code_dict):
+    encoded_sequence = ''
+    for char in sequence:
+        encoded_sequence += code_dict[char]
+    return encoded_sequence
+
+
+sequence = 'abcabaaabcedfabaccafc'
+subtrees = map_to_subtrees_list(sequence)
+pq = BinaryHeap(subtrees)
+
+while len(pq) > 1:
+    left = pq.del_min()
+    right = pq.del_min()
+    parent = join(left, right)
+    pq.insert(parent)
+
+root = pq.del_min()
+print(root)
+
+code_dict = {}
+root.get_huffman_code(code_dict)
+print(code_dict)
+
+encoded_sequence = encode(sequence, code_dict)
+print(f'{sequence} = {encoded_sequence}')
